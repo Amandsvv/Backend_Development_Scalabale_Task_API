b8ce50e12fd3d83c5ed12aa110246b8d65fae76b
Aman Kumar <amandsvv4006@gmail.com>
2026-03-13 18:44:20 +0530

    Fix: convert submodules to folders (final)

---
+++ backend/src/middlewares/errorHandler.middleware.js
@@ -0,0 +1,17 @@
+const errorHandler = (err, req, res, next) => {
+
+  console.error("ERROR:", err); // for backend debugging
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    ...(process.env.NODE_ENV === "development" && {
+      stack: err.stack,
+    }),
+  });
+};
+
+export {errorHandler}
